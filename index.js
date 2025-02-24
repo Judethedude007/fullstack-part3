914905f92f174b2aea85861b65cd81071bc51dcc
jude thomas <judethomas2004.com>
2025-02-24 21:39:27 +0530

    3.4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const persons = [
+let persons = [
   { 
     "id": "1",
     "name": "Arto Hellas", 
@@ -45,6 +45,12 @@ app.get('/api/persons/:id', (req, res) => {
   } else {
     res.status(404).send({ error: 'Person not found' });
   }
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = req.params.id;
+  persons = persons.filter(p => p.id !== id);
+  res.status(204).end();
 });
 
 const PORT = 3001; // Ensure port number is 3001

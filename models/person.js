f2032de5a6a6d36e04e695d7deeb89505ea076d7
jude thomas <judethomas2004.com>
2025-03-03 14:20:56 +0530

    3.20

--- models/person.js
+++ models/person.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
 
+const phoneValidator = {
+  validator: function (v) {
+    return /^\d{2,3}-\d+$/.test(v) && v.length >= 8;
+  },
+  message: props => `${props.value} is not a valid phone number!`
+};
+
 const personSchema = new mongoose.Schema({
   name: { type: String, required: true, minlength: 3 },
-  number: { type: String, required: true }
+  number: { type: String, required: true, validate: phoneValidator }
 });
 
 // Convert `_id` to `id` and remove `__v`
